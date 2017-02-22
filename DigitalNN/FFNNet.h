20207f8856bc43e5bd965a8d7b7f9aaa252ba94d
OtherwiseSix <otherwise-six@live.com>
2017-02-22 00:49:45 -0500

    Depricated DataSet and Neurons as seperate objects for speed.

--- DigitalNN/FFNNet.h
+++ DigitalNN/FFNNet.h
@@ -1,13 +1,35 @@
 /*Header File for FFNNet.cpp
-* This will define everything in our glorious Feed Forward Neural Network!
+* This will define everything for our glorious 
+* Feed Forward Neural Network (FFNN) class!
 *
 * Author @ Alex vanKooten
-* Version: 1.0 (02.15.2017)                                                   */
+* Version: 0.9 (02.21.2017)                                                   */
 
 #pragma once
 #ifndef NNet
 #define NNet
 #include "Scanner.h"
 
+class FFNNet {
+
+private:
+	friend NNetTrainer;
+	
+	//number of each kind of neuron
+	int num_inputs;
+	int num_hidden;
+	int num_outputs;
+
+	//vectors of neuron types
+	double* input_neurons;
+	double* hidden_neurons;
+	double* output_neurons;
+
+	//weight matrices
+	double** input_hidden_weights;
+	double** hidden_output_weights;
+
+
+}
 
 #endif
